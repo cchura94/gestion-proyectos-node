1676761edb89edf44bd7d561af4ee54646644b07
cchura94 <cchura.cpc@gmail.com>
2021-03-22 22:25:37 -0400

    MQTT

---
+++ src/demo_cliente.js
@@ -0,0 +1,16 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('ws://127.0.0.1:8888')
+ 
+client.on('connect', function () {
+  client.subscribe('test', function (err) {
+    if (!err) {
+      client.publish('test', 'Hola Prueba')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  //client.end()
+})
